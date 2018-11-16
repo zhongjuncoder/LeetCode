d18de70662dcb6e01124d82d393a9027efc877c6
zhongjuncoder <1247660633@qq.com>
2018-11-16 08:37:45 +0800

    在一个已排好序的数组中查找和为指定值的两个数

--- src/SearchAlgorithm.java
+++ src/SearchAlgorithm.java
@@ -1,7 +1,9 @@
+import java.util.Arrays;
+
 public class SearchAlgorithm {
 
     public static void main(String[] args) {
-
+        System.out.println(Arrays.toString(findSumNumbers(new int[]{-5, -1, 0, 5, 9, 11, 13, 15, 22, 35, 46}, 31)));
     }
 
     /**
@@ -23,6 +25,27 @@ public class SearchAlgorithm {
             }
         }
         return -1;
+    }
+
+    /**
+     * 给定一个按升序排列好的数组，求出数组中相加等于sum的两个数
+     *
+     * @param array {-5, -1, 0, 5, 9, 11, 13, 15, 22, 35, 46}
+     * @param sum   31
+     * @return {9,22}
+     */
+    private static int[] findSumNumbers(int[] array, int sum) {
+        int i = 0, j = array.length - 1;
+        while (i <= j) {
+            if (array[i] + array[j] == sum) {
+                return new int[]{array[i], array[j]};
+            } else if (array[i] + array[j] < sum) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return null;
     }
 
 }
