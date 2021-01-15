42fdcac7fc05ed3b72c140dc19df1b290588ac91
juncoder <1247660633@qq.com>
2021-01-15 22:19:33 +0800

    完成二叉搜索树的范围和

--- src/tree/easy/TopToBottom.java
+++ src/tree/easy/TopToBottom.java
@@ -17,4 +17,26 @@ public class TopToBottom {
         return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
     }
 
+    /**
+     * 给定二叉搜索树的根结点 root，返回值位于范围 [low, high] 之间的所有结点的值的和。
+     * 输入：root = [10,5,15,3,7,null,18], low = 7, high = 15
+     * 输出：32
+     */
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if (root == null) {
+            return 0;
+        }
+        int sum = 0;
+        if (root.val >= low && root.val <= high) {
+            sum += root.val;
+        }
+        if (root.val >= low) {
+            sum += rangeSumBST(root.left, low, high);
+        }
+        if (root.val <= high) {
+            sum += rangeSumBST(root.right, low, high);
+        }
+        return sum;
+    }
+
 }
