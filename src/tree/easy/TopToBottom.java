6cc95dac3da854caea58a65c8392cdf732e66a61
juncoder <1247660633@qq.com>
2021-01-16 23:24:36 +0800

    完成二叉搜索树的第k大节点

--- src/tree/easy/TopToBottom.java
+++ src/tree/easy/TopToBottom.java
@@ -11,6 +11,17 @@ import java.util.List;
  */
 public class TopToBottom {
 
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+
+        TreeNode node1 = new TreeNode(1);
+        root.left = node1;
+        node1.right = new TreeNode(2);
+
+        root.right = new TreeNode(4);
+        System.out.println(kthLargest(root, 1));
+    }
+
     /**
      * 输入一棵二叉树的根节点，求该树的深度。从根节点到叶节点依次经过的节点（含根、叶节点）形成树的一条路径，最长路径的长度为树的深度。
      */
@@ -80,5 +91,20 @@ public class TopToBottom {
             return searchBST(root.left, val);
         }
     }
+
+    private static int count = 0;
+
+    /**
+     * 给定一棵二叉搜索树，请找出其中第k大的节点。
+     * https://leetcode-cn.com/problems/er-cha-sou-suo-shu-de-di-kda-jie-dian-lcof/
+     */
+    public static int kthLargest(TreeNode root, int k) {
+        if (root == null) {
+            return Integer.MIN_VALUE;
+        }
+        int result = kthLargest(root.right, k);
+        return (++count == k) ? root.val : (count > k) ? result : kthLargest(root.left, k);
+    }
+
 
 }
