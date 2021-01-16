3f5e56bc3b40d5af08c88d10c0e557a9e42b2796
juncoder <1247660633@qq.com>
2021-01-16 23:53:17 +0800

    完成重建递增顺序查找树

--- src/tree/easy/TopToBottom.java
+++ src/tree/easy/TopToBottom.java
@@ -106,5 +106,28 @@ public class TopToBottom {
         return (++count == k) ? root.val : (count > k) ? result : kthLargest(root.left, k);
     }
 
+    private TreeNode treeNode;
+
+    private TreeNode rootTree;
+
+    /**
+     * 给你一个树，请你 按中序遍历 重新排列树，使树中最左边的结点现在是树的根，并且每个结点没有左子结点，只有一个右子结点。
+     * https://leetcode-cn.com/problems/increasing-order-search-tree/
+     */
+    public TreeNode increasingBST(TreeNode root) {
+        if (root == null) {
+            return rootTree;
+        }
+        increasingBST(root.left);
+        if (treeNode == null) {
+            treeNode = new TreeNode(root.val);
+            rootTree = treeNode;
+        } else {
+            treeNode.right = new TreeNode(root.val);
+            treeNode = treeNode.right;
+        }
+        increasingBST(root.right);
+        return rootTree;
+    }
 
 }
