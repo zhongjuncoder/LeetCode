06c09483e089175557b447b31522499f2b78f68d
juncoder <1247660633@qq.com>
2021-01-20 00:01:45 +0800

    完成修建二叉搜索树

--- src/tree/easy/LeftToRight.java
+++ src/tree/easy/LeftToRight.java
@@ -317,4 +317,25 @@ public class LeftToRight {
         }
     }
 
+    /**
+     * 给你二叉搜索树的根节点 root ，同时给定最小边界low 和最大边界 high。
+     * 通过修剪二叉搜索树，使得所有节点的值在[low, high]中。
+     * 修剪树不应该改变保留在树中的元素的相对结构（即，如果没有被移除，原有的父代子代关系都应当保留）。
+     * https://leetcode-cn.com/problems/trim-a-binary-search-tree/
+     */
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val > high) {
+            return trimBST(root.left, low, high);
+        } else if (root.val < low) {
+            return trimBST(root.right, low, high);
+        } else {
+            root.left = trimBST(root.left, low, high);
+            root.right = trimBST(root.right, low, high);
+        }
+        return root;
+    }
+
 }
