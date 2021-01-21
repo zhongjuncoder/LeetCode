31eafe2b1748ddb6109d31713a0bf384a936c333
juncoder <1247660633@qq.com>
2021-01-21 23:10:35 +0800

    完成相同的树

--- src/tree/easy/TopToBottom.java
+++ src/tree/easy/TopToBottom.java
@@ -198,4 +198,19 @@ public class TopToBottom {
         return leftResult && rightResult;
     }
 
+    /**
+     * 给定两个二叉树，编写一个函数来检验它们是否相同。
+     * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+     * https://leetcode-cn.com/problems/same-tree/
+     */
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if ((p == null || q == null) || p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
 }
