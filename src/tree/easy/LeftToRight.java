b31eb3158aeccd7c652409cf70230166cafe7b3c
juncoder <1247660633@qq.com>
2021-01-23 23:42:19 +0800

    判断是否是对称二叉树

--- src/tree/easy/LeftToRight.java
+++ src/tree/easy/LeftToRight.java
@@ -416,4 +416,55 @@ public class LeftToRight {
         }
     }
 
+    /**
+     * 请实现一个函数，用来判断一棵二叉树是不是对称的。如果一棵二叉树和它的镜像一样，那么它是对称的。
+     * <p>
+     * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+     * <p>
+     *     1
+     *    / \
+     *   2   2
+     *  / \ / \
+     * 3  4 4  3
+     * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+     * <p>
+     *     1
+     *    / \
+     *   2   2
+     *    \   \
+     *    3    3
+     * https://leetcode-cn.com/problems/dui-cheng-de-er-cha-shu-lcof/
+     */
+    public boolean isSymmetric(TreeNode root) {
+        List<TreeNode> list = new LinkedList<>();
+        if (root != null) {
+            list.add(root);
+            while (!list.isEmpty()) {
+                int size = list.size();
+                int halfSize = size / 2;
+                for (int i = 0; i < size; i++) {
+                    TreeNode start = list.get(i);
+                    if (i < halfSize) {
+                        TreeNode end = list.get(size - 1 - i);
+                        if (start != null && end != null) {
+                            if (start.val != end.val) {
+                                return false;
+                            }
+                        } else if (start != null || end != null) {
+                            return false;
+                        }
+                    }
+                    if (start != null) {
+                        list.add(start.left);
+                        list.add(start.right);
+                    }
+                }
+
+                list.subList(0, size).clear();
+
+            }
+        }
+        return true;
+    }
+
 }
