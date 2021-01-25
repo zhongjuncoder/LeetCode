8a344f7925bf994445b6e8a526cf525852a6a7e0
juncoder <1247660633@qq.com>
2021-01-25 23:49:49 +0800

    完成判断是否存在根节点到叶子节点的路径总和是否等于目标值

--- src/tree/easy/TopToBottom.java
+++ src/tree/easy/TopToBottom.java
@@ -381,4 +381,27 @@ public class TopToBottom {
         }
     }
 
+    /**
+     * 给你二叉树的根节点 root 和一个表示目标和的整数 targetSum ，判断该树中是否存在 根节点到叶子节点 的路径，这条路径上所有节点值相加等于目标和 targetSum 。
+     * <p>
+     * 叶子节点 是指没有子节点的节点。
+     * <p>
+     * 示例 1：
+     * <p>
+     * 输入：root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+     * 输出：true
+     * <p>
+     * https://leetcode-cn.com/problems/path-sum/
+     */
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null && root.val == targetSum) {
+            return true;
+        }
+        int sum = targetSum - root.val;
+        return hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
+    }
+
 }
