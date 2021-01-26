06d93b429b8ec1032684bf6b20d0bf1d215bd167
juncoder <1247660633@qq.com>
2021-01-27 00:18:35 +0800

    二叉树中第二小的节点

--- src/tree/easy/TopToBottom.java
+++ src/tree/easy/TopToBottom.java
@@ -404,4 +404,48 @@ public class TopToBottom {
         return hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
     }
 
+    /**
+     * 给定一个非空特殊的二叉树，每个节点都是正数，并且每个节点的子节点数量只能为 2 或 0。如果一个节点有两个子节点的话，那么该节点的值等于两个子节点中较小的一个。
+     * <p>
+     * 更正式地说，root.val = min(root.left.val, root.right.val) 总成立。
+     * <p>
+     * 给出这样的一个二叉树，你需要输出所有节点中的第二小的值。如果第二小的值不存在的话，输出 -1 。
+     * <p>
+     *  
+     * <p>
+     * 示例 1：
+     * <p>
+     * <p>
+     * 输入：root = [2,2,5,null,null,5,7]
+     * 输出：5
+     * 解释：最小的值是 2 ，第二小的值是 5 。
+     * <p>
+     * https://leetcode-cn.com/problems/second-minimum-node-in-a-binary-tree/
+     */
+    public int findSecondMinimumValue(TreeNode root) {
+        if (root == null) {
+            return -1;
+        }
+        return findSecondMinimumValueRecursion(root, root.val);
+    }
+
+    public int findSecondMinimumValueRecursion(TreeNode root, int currentMin) {
+        if (root != null) {
+            if (root.val > currentMin) {
+                return root.val;
+            } else if (root.left != null || root.right != null) {
+                int left = findSecondMinimumValueRecursion(root.left, currentMin);
+                int right = findSecondMinimumValueRecursion(root.right, currentMin);
+                if (left == -1) {
+                    return right;
+                }
+                if (right == -1) {
+                    return left;
+                }
+                return Math.min(left, right);
+            }
+        }
+        return -1;
+    }
+
 }
