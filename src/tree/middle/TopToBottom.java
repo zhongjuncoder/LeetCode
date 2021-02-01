ece1491e9a3bedfe8701c7a8b30171824bc595fd
juncoder <1247660633@qq.com>
2021-02-01 23:55:01 +0800

    祖父节点值为偶数的节点和

--- src/tree/middle/TopToBottom.java
+++ src/tree/middle/TopToBottom.java
@@ -94,4 +94,34 @@ public class TopToBottom {
         deepestLeavesSum(treeNode.right, nextDepth);
     }
 
+    /**
+     * 给你一棵二叉树，请你返回满足以下条件的所有节点的值之和：
+     * <p>
+     * 该节点的祖父节点的值为偶数。（一个节点的祖父节点是指该节点的父节点的父节点。）
+     * 如果不存在祖父节点值为偶数的节点，那么返回 0 。
+     * <p>
+     * 输入：root = [6,7,8,2,7,1,3,9,null,1,4,null,null,null,5]
+     * 输出：18
+     * 解释：图中红色节点的祖父节点的值为偶数，蓝色节点为这些红色节点的祖父节点。
+     * <p>
+     * 链接：https://leetcode-cn.com/problems/sum-of-nodes-with-even-valued-grandparent
+     */
+    public int sumEvenGrandparent(TreeNode root) {
+        sumEvenGrandparent(root, false, false);
+        return mSum;
+    }
+
+    private void sumEvenGrandparent(TreeNode treeNode, boolean grandParentIsEven, boolean parentIsEven) {
+        if (treeNode == null) {
+            return;
+        }
+        if (grandParentIsEven) {
+            mSum += treeNode.val;
+        }
+
+        boolean isEven = treeNode.val % 2 == 0;
+        sumEvenGrandparent(treeNode.left, parentIsEven, isEven);
+        sumEvenGrandparent(treeNode.right, parentIsEven, isEven);
+    }
+
 }
