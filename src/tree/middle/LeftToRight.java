e060e1bfb513d95cdaee9b134262d46e3ad53475
juncoder <1247660633@qq.com>
2021-02-02 23:06:31 +0800

    翻转等价二叉树

--- src/tree/middle/LeftToRight.java
+++ src/tree/middle/LeftToRight.java
@@ -139,4 +139,35 @@ public class LeftToRight {
         return depth;
     }
 
+    /**
+     * 我们可以为二叉树 T 定义一个翻转操作，如下所示：选择任意节点，然后交换它的左子树和右子树。
+     * <p>
+     * 只要经过一定次数的翻转操作后，能使 X 等于 Y，我们就称二叉树 X 翻转等价于二叉树 Y。
+     * <p>
+     * 编写一个判断两个二叉树是否是翻转等价的函数。这些树由根节点 root1 和 root2 给出。
+     * <p>
+     * 示例：
+     * <p>
+     * 输入：root1 = [1,2,3,4,5,6,null,null,null,7,8], root2 = [1,3,2,null,6,4,5,null,null,null,null,8,7]
+     * 输出：true
+     * 解释：我们翻转值为 1，3 以及 5 的三个节点。
+     * <p>
+     * 提示：
+     * <p>
+     * 每棵树最多有 100 个节点。
+     * 每棵树中的每个值都是唯一的、在 [0, 99] 范围内的整数。
+     * <p>
+     * 链接：https://leetcode-cn.com/problems/flip-equivalent-binary-trees
+     */
+    public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) {
+            return true;
+        } else if (root1 == null || root2 == null) {
+            return false;
+        } else if (root1.val != root2.val) {
+            return false;
+        }
+        return (flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right)) || (flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left));
+    }
+
 }
