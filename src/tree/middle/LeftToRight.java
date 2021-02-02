097c773685e2867d6c261c85482cbbf4a758447b
juncoder <1247660633@qq.com>
2021-02-02 22:48:57 +0800

    最大层内元素之和

--- src/tree/middle/LeftToRight.java
+++ src/tree/middle/LeftToRight.java
@@ -91,4 +91,52 @@ public class LeftToRight {
         return listNodes.toArray(new ListNode[listNodes.size()]);
     }
 
+    /**
+     * 给你一个二叉树的根节点 root。设根节点位于二叉树的第 1 层，而根节点的子节点位于第 2 层，依此类推。
+     * <p>
+     * 请你找出层内元素之和 最大 的那几层（可能只有一层）的层号，并返回其中 最小 的那个。
+     * <p>
+     * 输入：root = [1,7,0,7,-8,null,null]
+     * 输出：2
+     * 解释：
+     * 第 1 层各元素之和为 1，
+     * 第 2 层各元素之和为 7 + 0 = 7，
+     * 第 3 层各元素之和为 7 + -8 = -1，
+     * 所以我们返回第 2 层的层号，它的层内元素之和最大。
+     * <p>
+     * 链接：https://leetcode-cn.com/problems/maximum-level-sum-of-a-binary-tree
+     */
+    public int maxLevelSum(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int depth = 0;
+        int maxSum = Integer.MIN_VALUE;
+        List<TreeNode> treeNodeList = new LinkedList<>();
+        treeNodeList.add(root);
+
+        int floor = 0;
+        while (!treeNodeList.isEmpty()) {
+            floor++;
+            int size = treeNodeList.size();
+            int sum = 0;
+            for (int i = 0; i < size; i++) {
+                TreeNode treeNode = treeNodeList.remove(0);
+                sum += treeNode.val;
+
+                if (treeNode.left != null) {
+                    treeNodeList.add(treeNode.left);
+                }
+                if (treeNode.right != null) {
+                    treeNodeList.add(treeNode.right);
+                }
+            }
+            if (sum > maxSum) {
+                maxSum = sum;
+                depth = floor;
+            }
+        }
+        return depth;
+    }
+
 }
